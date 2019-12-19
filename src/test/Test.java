6eebde442f352822a89a5d5bc8e6e6d66da7793f
Linton <649557938@qq.com>
2019-12-19 17:06:36 +0800

    second commit

--- src/test/Test.java
+++ src/test/Test.java
@@ -13,12 +13,6 @@ public class Test {
     public static void main(String[] args) {
 
 
-        Integer[] aa = {2,34,5,6};
-        List<Integer> list = Arrays.asList(aa);
-        System.out.println(list.get(3));
-        list.remove(3);
-
-
 
     }
 }
